0236add03277fac405301bae8fc807d57c40100f
huynhtri123 <huynhmtri1211@gmail.com>
2024-09-15 11:10:33 +0700

    first commit

---
+++ src/main/java/app/sportcenter/SportcenterApplication.java
@@ -0,0 +1,13 @@
+package app.sportcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportcenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportcenterApplication.class, args);
+    }
+
+}
