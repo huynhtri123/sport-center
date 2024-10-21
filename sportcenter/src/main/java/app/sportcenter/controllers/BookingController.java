a90ff384e2d2cc6f8d7adbe2089e01921c33f5f1
huynhtri123 <126040259+huynhtri123@users.noreply.github.com>
2024-10-21 22:43:28 +0700

    feat (BE): complete Booking management (#13)

--- sportcenter/src/main/java/app/sportcenter/controllers/BookingController.java
+++ sportcenter/src/main/java/app/sportcenter/controllers/BookingController.java
@@ -2,15 +2,16 @@ package app.sportcenter.controllers;
 
 import app.sportcenter.commons.BaseResponse;
 import app.sportcenter.models.dto.BookingRequest;
-import app.sportcenter.models.dto.BookingResponse;
 import app.sportcenter.services.BookingService;
+import jakarta.servlet.ServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.time.ZonedDateTime;
+
 
 @RestController
 @RequestMapping("/api/booking")
@@ -22,6 +23,62 @@ public class BookingController {
     @PostMapping("/create")
     public ResponseEntity<BaseResponse> createBooking(@Valid @RequestBody BookingRequest bookingRequest) {
         return bookingService.createBooking(bookingRequest);
+    }
+
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
+    @GetMapping("/{bookingId}")
+    public ResponseEntity<BaseResponse> getBookingById(@PathVariable String bookingId) {
+        return bookingService.getBookingById(bookingId);
+    }
+
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
+    @GetMapping("/getByUserId/{userId}")
+    public ResponseEntity<BaseResponse> getByUserId(@PathVariable String userId) {
+        return bookingService.getBookingByUserId(userId);
+    }
+
+    @PreAuthorize("hasAnyAuthority('CUSTOMER')")
+    @GetMapping("/myBookings/{userId}")
+    public ResponseEntity<BaseResponse> getCurrentBookingsOfCurrentUser(@PathVariable String userId) {
+        return bookingService.getCurrentBookingsOfCurrentUser(userId);
+    }
+
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
+    @GetMapping("/getByFieldId/{fieldId}")
+    public ResponseEntity<BaseResponse> getByFieldId(@PathVariable String fieldId) {
+        return bookingService.getBookingByFieldId(fieldId);
+    }
+
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
+    @GetMapping("/byStartTime/{startTime}")
+    public ResponseEntity<BaseResponse> getBookingsByStartTime(@PathVariable("startTime") ZonedDateTime startTime) {
+        return bookingService.getBookingsByStartTime(startTime);
+    }
+
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
+    @GetMapping("/getAllActive")
+    public ResponseEntity<BaseResponse> getBookingsByStartTime() {
+        return bookingService.getAllBookings();
+    }
+
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
+    @PatchMapping("/softDelete/{bookingId}")
+    public ResponseEntity<BaseResponse> softDelete(@PathVariable String bookingId) {
+        boolean newIsDeleted = true;
+        return bookingService.changeIsDeleted(bookingId, newIsDeleted);
+    }
+
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
+    @PatchMapping("/restore/{bookingId}")
+    public ResponseEntity<BaseResponse> restore(@PathVariable String bookingId) {
+        boolean newIsDeleted = false;
+        return bookingService.changeIsDeleted(bookingId, newIsDeleted);
+    }
+
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
+    @DeleteMapping("/forceDelete/{bookingId}")
+    public ResponseEntity<BaseResponse> forceDelete(@PathVariable String bookingId) {
+        return bookingService.forceDelete(bookingId);
     }
 
 }
--- sportcenter/src/main/java/app/sportcenter/repositories/BookingRepository.java
+++ sportcenter/src/main/java/app/sportcenter/repositories/BookingRepository.java
@@ -7,11 +7,27 @@ import org.springframework.stereotype.Repository;
 
 import java.time.ZonedDateTime;
 import java.util.List;
+import java.util.ResourceBundle;
 
 @Repository
 public interface BookingRepository extends MongoRepository<Booking, String> {
 
-    // tìm danh sách sân hết hạn và trạng thái đang được thuê (endTime<now & status=IN_USE)
-    @Query("{ 'endTime' : { $lt: ?0 }, 'field.fieldStatus' : ?1 }")
-    List<Booking> findExpiredBookings(ZonedDateTime now, String fieldStatus);
+    @Query("{ 'isActive': true, 'isDeleted': false }")
+    public List<Booking> getAllActive();
+
+    // tìm danh sách sân hết hạn và trạng thái đang được thuê (endTime<now)
+    @Query("{ 'endTime' : { $lt: ?0 }, 'field.fieldStatus' : ?1, 'isActive': true, 'isDeleted': false }")
+    public List<Booking> findExpiredBookings(ZonedDateTime now, String fieldStatus);
+
+    @Query("{ 'user._id':  ?0, 'isActive': true, 'isDeleted': false }")
+    public List<Booking> findBookingByUserId(String userId);
+
+    @Query("{ 'user._id':  ?0, 'endTime' : { $gt: ?1 }, 'field.fieldStatus' : ?2, 'isActive': true, 'isDeleted': false }")
+    public List<Booking> getCurrentBookingsOfCurrentUser(String userId, ZonedDateTime now, String fieldStatus);
+
+    @Query("{ 'field._id':  ?0, 'isActive': true, 'isDeleted': false }")
+    public List<Booking> getBookingByFieldId(String fieldId);
+
+    @Query("{ 'startTime': ?0, 'isActive': true, 'isDeleted': false }")
+    public List<Booking> getBookingByStartTime(ZonedDateTime startTime);
 }
