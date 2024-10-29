078cdcad2a185a41ce624c5551cf9f93e30fd08b
huynhtri123 <huynhmtri1211@gmail.com>
2024-10-29 16:55:22 +0700

    refactor: comment out Tournament logic for upcoming improvements

--- sportcenter/src/main/java/app/sportcenter/controllers/TournamentController.java
+++ sportcenter/src/main/java/app/sportcenter/controllers/TournamentController.java
@@ -12,42 +12,42 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/tounament")
 public class TournamentController {
-    @Autowired
-    private TournamentService tournamentService;
-
-    @PreAuthorize("hasAuthority('ADMIN')")
-    @PostMapping("/create")
-    public ResponseEntity<BaseResponse> create(@Valid @RequestBody TournamentRequest tournamentRequest) {
-        return tournamentService.create(tournamentRequest);
-    }
-
-    @PreAuthorize("hasAuthority('ADMIN')")
-    @GetMapping("/getById/{id}")
-    public ResponseEntity<BaseResponse> getById(@PathVariable String id) {
-        return tournamentService.getById(id);
-    }
-
-    @PreAuthorize("hasAuthority('ADMIN')")
-    @PutMapping("/update/{tounamentId}")
-    public ResponseEntity<BaseResponse> update(@PathVariable String tounamentId, @Valid @RequestBody TournamentRequest tournamentRequest) {
-        return tournamentService.update(tounamentId, tournamentRequest);
-    }
-
-    @PreAuthorize("hasAuthority('ADMIN')")
-    @PatchMapping("/softDelete/{tounamentId}")
-    public ResponseEntity<BaseResponse> softDelete(@PathVariable String tounamentId) {
-        return tournamentService.delete(tounamentId);
-    }
-
-    @PreAuthorize("hasAuthority('ADMIN')")
-    @GetMapping("/getAll")
-    public ResponseEntity<BaseResponse> getAll() {
-        return tournamentService.getAll();
-    }
-
-    @PreAuthorize("hasAuthority('ADMIN')")
-    @PatchMapping("/restore/{tounamentId}")
-    public ResponseEntity<BaseResponse> restore(@PathVariable String tounamentId) {
-        return tournamentService.restore(tounamentId);
-    }
+//    @Autowired
+//    private TournamentService tournamentService;
+//
+//    @PreAuthorize("hasAuthority('ADMIN')")
+//    @PostMapping("/create")
+//    public ResponseEntity<BaseResponse> create(@Valid @RequestBody TournamentRequest tournamentRequest) {
+//        return tournamentService.create(tournamentRequest);
+//    }
+//
+//    @PreAuthorize("hasAuthority('ADMIN')")
+//    @GetMapping("/getById/{id}")
+//    public ResponseEntity<BaseResponse> getById(@PathVariable String id) {
+//        return tournamentService.getById(id);
+//    }
+//
+//    @PreAuthorize("hasAuthority('ADMIN')")
+//    @PutMapping("/update/{tounamentId}")
+//    public ResponseEntity<BaseResponse> update(@PathVariable String tounamentId, @Valid @RequestBody TournamentRequest tournamentRequest) {
+//        return tournamentService.update(tounamentId, tournamentRequest);
+//    }
+//
+//    @PreAuthorize("hasAuthority('ADMIN')")
+//    @PatchMapping("/softDelete/{tounamentId}")
+//    public ResponseEntity<BaseResponse> softDelete(@PathVariable String tounamentId) {
+//        return tournamentService.delete(tounamentId);
+//    }
+//
+//    @PreAuthorize("hasAuthority('ADMIN')")
+//    @GetMapping("/getAll")
+//    public ResponseEntity<BaseResponse> getAll() {
+//        return tournamentService.getAll();
+//    }
+//
+//    @PreAuthorize("hasAuthority('ADMIN')")
+//    @PatchMapping("/restore/{tounamentId}")
+//    public ResponseEntity<BaseResponse> restore(@PathVariable String tounamentId) {
+//        return tournamentService.restore(tounamentId);
+//    }
 }
